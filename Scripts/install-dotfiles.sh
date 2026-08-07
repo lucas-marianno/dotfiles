e99d4317f84cc48066e1ff054687e520e3e01618
Lucas Marianno <lucas.marianno94@gmail.com>
2026-08-06 23:23:24 -0300

    add ashell to install script

--- Scripts/install-dotfiles.sh
+++ Scripts/install-dotfiles.sh
@@ -2,16 +2,15 @@
 
 echo "Creating symlinks from dotfiles to ~/.config"
 
-
-
 ln -s $(pwd)/Thunar/ ~/.config/
 ln -s $(pwd)/htop/ ~/.config/
 ln -s $(pwd)/hypr/ ~/.config/
+ln -s $(pwd)/ashell/ ~/.config/
 ln -s $(pwd)/kitty/ ~/.config/
 ln -s $(pwd)/nvim/ ~/.config/
 ln -s $(pwd)/tmux/ ~/.config/
 
-echo "Creating symlinks from Scrips to dotfiles/Scripts"
+echo "Creating symlinks from Scripts to dotfiles/Scripts"
 
 ln -s $(pwd)/Scripts/ ~/
 
