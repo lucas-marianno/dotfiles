81ea2b898a6da8437321f7d6f3fad819c291b95c
Lucas Marianno <120429072+lucas-marianno@users.noreply.github.com>
2026-09-25 16:25:47 -0300

    Change catppuccin's background to #000000
    Also, disable lazyvim's dashboard

---
+++ nvim/lua/plugins/colorscheme-catppuccin.lua
@@ -0,0 +1,14 @@
+return {
+  "catppuccin/nvim",
+  name = "catppuccin",
+  opts = {
+    flavour = "mocha",
+    color_overrides = {
+      mocha = {
+        base = "#000000",
+        mantle = "#000000",
+        crust = "#000000"
+      }
+    }
+  }
+}
